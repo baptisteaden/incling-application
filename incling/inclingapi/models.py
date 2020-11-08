00e11c7a1af9a14b15076e0078ff9bcc6bd61d01
kigamii <baptiste.aden@gmail.com>
2020-11-08 18:32:48 +0100

    update models

--- incling/inclingapi/models.py
+++ incling/inclingapi/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class Tile(models.Model):
     TILE_STATUSES = (
         ('live', 'Live'),
         ('pending', 'Pending'),
         ('archived', 'Archived')
     )
-    launchDate = models.DateField
+    launchDate = models.DateField()
     status = models.CharField(max_length=8, choices=TILE_STATUSES)
+
 
 class Task(models.Model):
     TASK_TYPES = (
@@ -16,7 +18,7 @@ class Task(models.Model):
         ('diary', 'Diary')
     )
     title = models.CharField(max_length=50)
-    order = models.CharField(max_length=10)
-    description = models.CharField(max_length=255)
+    order = models.DecimalField(max_digits=10, decimal_places=0)
+    description = models.CharField(max_length=255, blank=True)
     type = models.CharField(max_length=10, choices=TASK_TYPES)
     tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
