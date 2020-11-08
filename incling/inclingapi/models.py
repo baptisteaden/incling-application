fc87318592dea66d7ce8241b4938023164384458
Baptiste Aden <baptiste.aden@viavisolutions.com>
2020-11-08 13:28:41 +0100

    update dependencies versions + add static files

--- incling/inclingapi/models.py
+++ incling/inclingapi/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 class Tile(models.Model):
-    TileStatus = models.TextChoices('TileStatus', 'LIVE PENDING ARCHIVED')
+    TILE_STATUSES = (
+        ('live', 'Live'),
+        ('pending', 'Pending'),
+        ('archived', 'Archived')
+    )
     launchDate = models.DateField
-    status = models.CharField(max_length=8, choices=TileStatus.choices)
+    status = models.CharField(max_length=8, choices=TILE_STATUSES)
 
 class Task(models.Model):
-    TaskType = models.TextChoices('TaskType', 'SURVEY DISCUSSION DIARY')
+    TASK_TYPES = (
+        ('survey', 'Survey'),
+        ('discussion', 'Discussion'),
+        ('diary', 'Diary')
+    )
     title = models.CharField(max_length=50)
     order = models.CharField(max_length=10)
     description = models.CharField(max_length=255)
-    type = models.CharField(max_length=10, choices=TaskType.choices)
+    type = models.CharField(max_length=10, choices=TASK_TYPES)
     tile = models.ForeignKey(Tile, on_delete=models.CASCADE)
